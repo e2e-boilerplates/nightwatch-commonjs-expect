3788fe1c7d2534f29441b881584e4c504218c880
Girma Nigusse <xgirma@gmail.com>
2020-03-26 23:51:14 -0400

    update

--- spec/sandbox.spec.js
+++ spec/sandbox.spec.js
@@ -1,14 +1,14 @@
 module.exports = {
-  before: browser => {
+  before: (browser) => {
     browser.url("https://e2e-boilerplate.github.io/sandbox/");
   },
 
-  after: browser => {
+  after: (browser) => {
     browser.end();
   },
 
-  "should be on Sandbox": function(browser) {
+  "should be on Sandbox": function (browser) {
     browser.expect.title().to.contain("Sandbox");
     browser.expect.element("h1").text.to.equal("Sandbox");
-  }
+  },
 };
